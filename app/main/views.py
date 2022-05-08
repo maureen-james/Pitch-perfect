5b728c5aad3c8571f1ca4894abe34dabe3358cd7
maureen-james <manjax97@gmail.com>
2022-05-08 10:53:45 +0300

    add packages dependencies in requirements file

--- app/main/views.py
+++ app/main/views.py
@@ -5,6 +5,8 @@ from main import main_blueprint as main
 @main.route('/')
 def index():
     '''
-     View root page function that returns the index page and its data
+    View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+    

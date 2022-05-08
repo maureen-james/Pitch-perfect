7e8a72c995cc5f5d519406dfb3e133ccbfc457ba
maureen-james <manjax97@gmail.com>
2022-05-09 01:54:35 +0300

    add pitch models

--- app/models.py
+++ app/models.py
@@ -27,3 +27,14 @@ class User(UserMixin, db.Model):
     downvote = db.relationship('Downvote', backref='user', lazy='dynamic')
     profile_pic_path = db.Column(db.String())
 
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255), nullable = False)
+    post = db.Column(db.Text(), nullable = False)
+    comment = db.relationship('Comment', backref='pitch', lazy='dynamic')
+    category = db.Column(db.String(255), index = True,nullable = False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    upvote = db.relationship('Upvote', backref='pitch', lazy='dynamic')
+    downvote = db.relationship('Downvote', backref='pitch', lazy='dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))

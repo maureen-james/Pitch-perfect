5ffbe1439374db7fb9ddd8677961b9e6e5e85abb
maureen-james <manjax97@gmail.com>
2022-05-08 08:14:21 +0300

    modify the views app

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,10 @@
 from flask import render_template
+from . import main
 from main import main_blueprint as main
 
 @main.route('/')
 def index():
-     render_template('index.html')
+    '''
+     View root page function that returns the index page and its data
+    '''
+    return render_template('index.html')

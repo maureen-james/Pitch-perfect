99dfd51093d54842a2fd491a701787f56e735924
maureen-james <manjax97@gmail.com>
2022-05-08 14:07:18 +0300

    create and add 404 file

--- app/models_test.py
+++ app/models_test.py
@@ -1,2 +1,27 @@
 import unittest
 from app import models
+
+# class SourceTest(unittest.TestCase):
+#     '''
+#     Test Class to test the behaviour of the News class
+#     '''
+
+#     def setUp(self):
+#         '''
+#         Set up method that will run before every Test
+#         '''
+#         self.new_Source = Source("abc-news","ABC News","Your trusted source for breaking news", "analysis, exclusive interviews, headlines, and videos at ABCNews.com.", "https://abcnews.go.com", "general", "en", "us")
+
+#     def test_instance(self):
+#         self.assertTrue(isinstance(self.new_Source,Source))
+
+# class TestArticle(unittest.TestCase):
+#     def setUp(self):
+#         self.new_article=Article('by maureen','CNN','killer cat','www.cnn.com','from','skjsdjfkd.jpg','12/12/15','killer',)
+
+#     def test_instance(self):
+#         self.assertTrue(isinstance(self.new_article,Article))        
+
+
+if __name__ == '__main__':
+    unittest.main()

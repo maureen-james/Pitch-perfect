d313e15da86732f4715e928df457fee0acb1b474
maureen-james <manjax97@gmail.com>
2022-05-11 20:15:42 +0300

    add secret key and add it to gitignore

--- config.py
+++ config.py
@@ -5,7 +5,20 @@ class Config:
     General configuration parent class
     '''
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:maureen@localhost/pitch'
-    pass
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    @staticmethod
+    def init_app(app):
+     pass
 
 class ProdConfig(Config):
     '''

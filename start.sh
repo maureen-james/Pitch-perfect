2ff9cbe6b0c279eab13ed387b0b46dd78cb7ae72
maureen-james <manjax97@gmail.com>
2022-05-12 01:00:33 +0300

    Staging for deployment to Heroku

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
-export MAIL_USERNAME=<manjax97@gmail.com>
-export MAIL_PASSWORD=<maureennewton>
+export MAIL_USERNAME=manjax97@gmail.com
+export MAIL_PASSWORD=maureennewton
+export SECRET_KEY = m1N2Y3a4M5b6U7r8A9
 python3 manage.py server
